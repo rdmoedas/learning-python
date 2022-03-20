a7538e6bfcccb4d1c1cbb528e175467fbd8978b4
Rodolfo Santana <rdmoedas@gmail.com>
2022-03-20 02:00:02 -0300

    feat: add Hello World and some import test

---
+++ run.py
@@ -0,0 +1,22 @@
+# First import method
+# import test.first
+
+# test.first.print_hello_world()
+
+######
+# Second import method
+# from test import first
+
+# first.print_hello_world()
+
+######
+# Third import method
+# from test.first import print_hello_world
+
+# print_hello_world()
+
+# There are many other options like:
+
+import test.first as FIRST
+
+FIRST.print_hello_world()
